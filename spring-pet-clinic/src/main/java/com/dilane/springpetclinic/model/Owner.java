88872c68e819b4c629b82f0e6afd3ba7218540d3
Dilane-Kamga <dilanekamga777@hotmail.com>
2023-06-14 23:01:06 +0100

    adding POJOs. Closes #2.

---
+++ spring-pet-clinic/src/main/java/com/dilane/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.dilane.springpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ spring-pet-clinic/src/main/java/com/dilane/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.dilane.springpetclinic.model;
+
+public class Vet extends Person{
+
+}
